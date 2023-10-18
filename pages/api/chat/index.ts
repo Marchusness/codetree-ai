b74bec9edd734271ed9ab216a74e0c76f74fa172
Matt Hoffner <matthoffner@gmail.com>
2023-10-18 15:58:28 -0700

    Add system prompt

--- pages/api/chat/index.ts
+++ pages/api/chat/index.ts
@@ -9,7 +9,15 @@ const openai = new OpenAIApi(config);
 export const runtime = "edge";
 
 export default async function(req: Request) {
-    const { messages } = await req.json()
+    let { messages } = await req.json();
+
+    // Prepend the system message if it's not already there
+    if (messages.length === 0 || messages[0].role !== "system") {
+        messages = [{
+            role: "system",
+            content: "You are a helpful AI assistant. Assist the user in writing React code, ensuring that all logic is contained within a single app component file and that the output is rendered to the DOM's root element."
+        }, ...messages];
+    }
 
     const response = await openai.createChatCompletion({
         model: 'gpt-4',
@@ -17,7 +25,6 @@ export default async function(req: Request) {
         messages
     })
 
-    const stream = OpenAIStream(response)
-    return new StreamingTextResponse(stream)
+    const stream = OpenAIStream(response);
+    return new StreamingTextResponse(stream);
 }
-
